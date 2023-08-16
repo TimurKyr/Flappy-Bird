c19c953d516b82c92cffe0f7eb026842d9031ba8
TimurKyr <kyrbasovtimur@gmail.com>
2023-08-16 20:44:40 +0600

    added score and modified images

--- main.js
+++ main.js
@@ -1,12 +1,13 @@
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
-const heightId = document.getElementById("height");
+const scoreId = document.getElementById("score");
 
 var bird = new Image();
 var bg = new Image();
 var fg = new Image();
 var pipeUp = new Image();
 var pipeBottom = new Image();
+var score = 0;
 
 bird.src = "assets/images/bird.png"
 bg.src = "assets/images/bg.png"
@@ -14,7 +15,7 @@ fg.src = "assets/images/fg.png"
 pipeUp.src = "assets/images/pipeUp.png"
 pipeBottom.src = "assets/images/pipeBottom.png"
 
-const gap = 140;
+const gap = 150;
 
 document.addEventListener("keydown", moveUp);
 
@@ -30,14 +31,22 @@ pipe[0] = {
 
 function draw() {
     ctx.drawImage(bg, 0, 0);
-    heightId.textContent = "Height = " + yPos;
+    scoreId.textContent = "Score: " + score;
 
     for(var i = 0; i < pipe.length; i ++) {
+        if(pipe[i].x + pipeUp.width < 0){
+            pipe.shift();
+        }
+
+        if(xPos == pipe[i].x + pipeUp.width) {
+            score += 1;
+        }
+
         ctx.drawImage(pipeUp, pipe[i].x, pipe[i].y);
         ctx.drawImage(pipeBottom, pipe[i].x, pipe[i].y + pipeUp.height + gap);
         pipe[i].x --;
 
-        if(pipe[i].x == 300) {
+        if(pipe[i].x == 240) {
             pipe.push({
                 x: canvas.width,
                 y: Math.floor(Math.random() * pipeUp.height) - pipeUp.height
@@ -51,7 +60,7 @@ function draw() {
             location.reload();
         }
 
-        if(yPos == canvas.height - fg.height - bird.height) {
+        if(yPos >= canvas.height - fg.height - bird.height) {
             location.reload();
         }
     }
